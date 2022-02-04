763471d22999dd6ebe073d96c300b5cbc354c503
abigailyuan <abigail_yuan@hotmail.com>
2022-02-04 12:53:00 +1100

    create 1% sample

--- sample_creation.py
+++ sample_creation.py
@@ -1,11 +1,16 @@
 import pickle
+import os
 
 directory = 'ProcessedWSJ/'
 files = ['wsj_cleaned.pkl', 'wsj_index.pkl','wsj_raw.pkl','wsj_stemmed.pkl','wsj_tokenized.pkl']
 
+os.mkdir('sample1WSJ')
 for file in files:
     filename = directory+file
     object = pickle.load(open(filename, 'rb'))
-    print(file)
-    print(type(object))
-    print(object[0])
+    sample = object[:len(object)//100]
+    pickle.dump(sample, open(directory+'sampleWSJ/sample'+file, 'wb'))
+    print(len(sample))
+    print(len(object))
+
+
