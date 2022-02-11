1798a34874b4297dad789ef1f3f2485c018128a8
abigailyuan <abigail_yuan@hotmail.com>
2022-02-11 12:44:49 +1100

    score

---
+++ pipeline.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+import preprocessing
+import corpus_vectorizer
+import pickle
+
+import KMeansGenerator
+import LDAGenerator
+import Visualisation
+
+
+order = 20
+cid = 7
+tid = 1
+
+clustering = 'ClusterResults/' + str(cid) + '/model'
+topic_model = 'LDAResults/' + str(tid) + '/model'
+corpus = 'ProcessedWSJ/tfidf_corpus.pkl'
+directory = 'figures/'
+clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,
+                                                                         order=order)
+
+centroids = KMeansGenerator.get_cluster_vectors(cid)
+topics = LDAGenerator.get_topic_vectors(tid)
+cos_sim_matrix = Visualisation.vector_similarity(centroids, topics)
+
+result = np.multiply(cluster_topic_matrix, cos_sim_matrix)
+print('Cluster scores:')
+print(np.sum(result, 1))
+
+print('/nTopic scores:')
+print(np.sum(result, 0))
