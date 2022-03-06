update figures and fix bug in dist computation

main.py
@@ -1,11 +1,12 @@
 import preprocessing
 import corpus_vectorizer
 import pickle
+from gensim.models import LdaModel
 
 import KMeansGenerator
 import LDAGenerator
 import Visualisation
-
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -102,21 +103,21 @@ def work_pipeline():
     #     print('LDA run: ' + str(tid) + ' generated.')
 
     # generate figures
-    # cid = 9
-    # tid = 1
-    # clustering = 'ClusterResults/' + str(cid) + '/model'
-    # topic_model = 'LDAResults/' + str(tid) + '/model'
-    # corpus = 'ProcessedWSJ/tfidf_corpus.pkl'
-    # directory = 'figures/'
-    # clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,
-    #                                                                      order=order, mode='distribution')
-    # Visualisation.topic_distribution_visualise(clusters, cluster_topic_matrix, cid=cid, tid=tid, order=order,
-    #                                            directory=directory, mode='distribution')
-    #
-    # clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,
-    #                                                                      order=order, mode='label')
-    # Visualisation.topic_distribution_visualise(clusters, cluster_topic_matrix, cid=cid, tid=tid, order=order,
-    #                                            directory=directory, mode='label')
+    cid = 9
+    tid = 1
+    clustering = 'ClusterResults/' + str(cid) + '/model'
+    topic_model = 'LDAResults/' + str(tid) + '/model'
+    corpus = 'ProcessedWSJ/tfidf_corpus.pkl'
+    directory = 'figures/'
+    clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,
+                                                                         order=order, mode='distribution')
+    Visualisation.topic_distribution_visualise(clusters, cluster_topic_matrix, cid=cid, tid=tid, order=order,
+                                               directory=directory, mode='distribution')
+
+    clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,
+                                                                         order=order, mode='label')
+    Visualisation.topic_distribution_visualise(clusters, cluster_topic_matrix, cid=cid, tid=tid, order=order,
+                                               directory=directory, mode='label')
 
     # vector similarity
     # cid = 9
@@ -155,14 +156,14 @@ def work_pipeline():
     topic_model = 'LDAResults/' + str(tid) + '/model'
     corpus = 'ProcessedWSJ/tfidf_corpus.pkl'
     directory = 'figures/'
-    clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,
-                                                                         order=order, mode='distribution')
+    # clusters, cluster_topic_matrix = Visualisation.compare_cluster_topic(clustering, topic_model, corpus=corpus,order=order, mode='distribution')
     # print(cluster_topic_matrix.head())
-    t = 15
-    dist = Visualisation.get_topic_distribution(corpus, cid, tid, c=0, t=t, mode='all')
-    print(dist[:100])
-    Visualisation.hist_plot(topic_dist=dist, c=1, t=t, tid=1, directory='figures/test/git ')
-
+    # t = 2
+    # dist = Visualisation.get_topic_distribution(corpus, cid, tid, c=0, t=t, mode='all')
+    # print(max(dist))
+    # print(np.mean(dist))
+    # print(np.median(dist))
+    # Visualisation.hist_plot(topic_dist=dist, c=1, t=t, tid=1, directory='figures/test/')
 
     # for t in range(20):
     #     dist = Visualisation.get_topic_distribution(corpus, cid, tid, c=0, t=t, mode='all')