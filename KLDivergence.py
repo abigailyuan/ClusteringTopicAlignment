3e9c3d54b24dadaac408f4dea90ce575d7790ef8
abigailyuan <myuan3@student.unimelb.edu.au>
2022-04-26 12:48:54 +1000

    finished KL divergence

--- KLDivergence.py
+++ KLDivergence.py
@@ -46,42 +46,66 @@ def get_domain(query_file):
     text = fp.read()
     fp.close()
     domains = dd(list)
+    queries_dict = {}
     queries = text.split('</top>')
+    for i in range(len(queries[:-1])):
+        queries[i] = queries[i].split('\n')
+        query_dict = {}
+        num=0
+        for row in queries[i]:
+            if '<dom>' in row:
+                query_dict['domain'] = row[14:].strip()
+            if '<num> ' in row:
+                num = int(row[14:])
+            if '<title>' in row:
+                query_dict['title'] = row[15:]
+        queries_dict[num] = query_dict
+    return queries_dict
 
-
-queries = get_query_docs()
-normalised_distribution, corpus_topics = get_collection_topic_distribution(tid=1, corpus_file='ProcessedWSJ/tfidf_corpus.pkl')
-query_kl = dd(float)
-for query in queries.keys():
-    # get query subset topic distribution
-    query_topics = get_subset_topic_distribution(corpus_topics, queries[query])
-
-    # compute KL divergence regarding to corpus distribution
-    kl_divergence = KL_divergence(normalised_distribution, query_topics)
-    query_kl[query] = kl_divergence
-
-query_kl_lst = sorted([(k,v) for k,v in query_kl.items()], key=lambda x:x[1])
-print(query_kl_lst[:5])
-x = [x[0] for x in query_kl_lst]
-y = [x[1] for x in query_kl_lst]
-
-fig, ax = plt.subplots(figsize=(20,10))
-ax.bar(np.arange(101,201), y, width=1, edgecolor="white")
-ax.set_xticks(np.arange(101,201),x)
-ax.set_xlabel('TREC topics', fontsize=18)
-ax.set_ylabel('KL Divergence', fontsize=18)
-ax.set_title("KL Divergence with reference to collection topic distribution", fontsize = 18)
-plt.xticks(rotation=90, fontsize=10)
-plt.savefig("figures/KL_divergence.pdf")
-plt.clf()
-
-fig, ax = plt.subplots(figsize=(20,10))
-x = [k for k,v in query_kl.items()]
-y = [v for k,v in query_kl.items()]
-ax.bar(x,y, width=1, edgecolor="white")
-ax.set_xlabel('TREC topics', fontsize=18)
-ax.set_ylabel('KL Divergence', fontsize=18)
-ax.set_xticks(np.arange(101,201),x)
-ax.set_title("KL Divergence with reference to collection topic distribution", fontsize = 18)
-plt.xticks(rotation=90, fontsize=10)
-plt.savefig("figures/KL_divergence_unsorted.pdf")
+queries = get_domain('topics.101-150')
+domains = dd(list)
+for i in queries:
+    domains[queries[i]['domain']].append(i)
+for k in domains:
+    print(k)
+print(len(domains))
+print(domains['International Politics'])
+print(domains['Military'])
+print(domains['International Economics'])
+print(domains['Medical & Biological'])
+# queries = get_query_docs()
+# normalised_distribution, corpus_topics = get_collection_topic_distribution(tid=1, corpus_file='ProcessedWSJ/tfidf_corpus.pkl')
+# query_kl = dd(float)
+# for query in queries.keys():
+#     # get query subset topic distribution
+#     query_topics = get_subset_topic_distribution(corpus_topics, queries[query])
+#
+#     # compute KL divergence regarding to corpus distribution
+#     kl_divergence = KL_divergence(normalised_distribution, query_topics)
+#     query_kl[query] = kl_divergence
+#
+# query_kl_lst = sorted([(k,v) for k,v in query_kl.items()], key=lambda x:x[1])
+# print(query_kl_lst[:5])
+# x = [x[0] for x in query_kl_lst]
+# y = [x[1] for x in query_kl_lst]
+#
+# fig, ax = plt.subplots(figsize=(20,10))
+# ax.bar(np.arange(101,201), y, width=1, edgecolor="white")
+# ax.set_xticks(np.arange(101,201),x)
+# ax.set_xlabel('TREC topics', fontsize=18)
+# ax.set_ylabel('KL Divergence', fontsize=18)
+# ax.set_title("KL Divergence with reference to collection topic distribution", fontsize = 18)
+# plt.xticks(rotation=90, fontsize=10)
+# plt.savefig("figures/KL_divergence.pdf")
+# plt.clf()
+#
+# fig, ax = plt.subplots(figsize=(20,10))
+# x = [k for k,v in query_kl.items()]
+# y = [v for k,v in query_kl.items()]
+# ax.bar(x,y, width=1, edgecolor="white")
+# ax.set_xlabel('TREC topics', fontsize=18)
+# ax.set_ylabel('KL Divergence', fontsize=18)
+# ax.set_xticks(np.arange(101,201),x)
+# ax.set_title("KL Divergence with reference to collection topic distribution", fontsize = 18)
+# plt.xticks(rotation=90, fontsize=10)
+# plt.savefig("figures/KL_divergence_unsorted.pdf")
