1b3db3a587ae1b3d11d50ca12ddf008c8101673d
Abby Yuan <abigail_yuan@hotmail.com>
2025-06-21 16:32:39 +1000

    delete outdated modules

--- model_creation/HierarchicalClusteringGenerator.py
+++
@@ -1,7 +0,0 @@
-from sklearn.cluster import AgglomerativeClustering
-
-def agglomaritive(vectorised_corpus):
-    model = AgglomerativeClustering(metric='euclidean', linkage='ward', compute_distances=True)
-    clustering = model.fit(vectorised_corpus)
-    return clustering
-
